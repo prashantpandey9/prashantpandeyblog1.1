a65a6fbaa176600e462a3a2a8ade21d7b0724d1c
prashantpandey9 <prashantpandey94551@gmail.com>
2020-08-19 20:28:32 +0530

    added all post

--- src/components/bio.js
+++ src/components/bio.js
@@ -7,10 +7,10 @@
 
 import React from "react"
 import { StaticQuery, graphql } from "gatsby"
-import Image from "gatsby-image"
+// import Image from "gatsby-image"
 import styled from "styled-components"
 
-import { rhythm } from "../utils/typography"
+// import { rhythm } from "../utils/typography"
 
 function Bio() {
   return (
@@ -20,7 +20,7 @@ function Bio() {
         const { author, social } = data.site.siteMetadata
         return (
           <Container>
-            <Image
+            {/* <Image
               fixed={data.avatar.childImageSharp.fixed}
               alt={author}
               style={{
@@ -32,7 +32,7 @@ function Bio() {
               imgStyle={{
                 borderRadius: `50%`,
               }}
-            />
+            /> */}
             <p>
               Written by <strong>{author}</strong>
               {` `}
